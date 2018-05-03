5550b0b8b58b96325308fd55fcef54658d3b33f0
Too <weiweitoo98@gmail.com>
2018-05-03 21:35:08 +0800

    fixing ajax

--- asset/js/app.js
+++ asset/js/app.js
@@ -165,7 +165,7 @@ function indexReady(){
 function projectReady(){
 	$('body').css('overflow','scroll');
 	$("html, body").animate({ scrollTop: 0 });
-	$('.item-title').css('opacity',1);
+	$('.item-title')[0].css('opacity',1);
 	console.log($('.item-title'));
 	setTimeout(function(){
 		$('.item-content').css('opacity',1);
